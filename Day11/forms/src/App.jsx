bda87b2b36544b99cfeaadab29046bf87f86eaf0
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-16 19:07:12 +0530

    forms

--- Day11/forms/src/App.jsx
+++ Day11/forms/src/App.jsx
@@ -6,22 +6,36 @@ import React ,{useState}from "react"
 function App() {
 
 const [text,setText] = useState("")
-const [fullname,setFullname] = useState()
+const [text2,setText2] = useState("")
+
+const [firstname,setFirstname] = useState()
+const [lastnamenew,setLastnameNew] = useState()
+
 
 const textChange = (event) => {
   setText(event.target.value)
 }
-const nameChange = () => {
-  setFullname(text)
+
+const  textChange2 = (event) => {
+  setText2(event.target.value)
+}
+const onSubmits = (event) => {
+  event.preventDefault()
+  setFirstname(text)
+  setLastnameNew(text2)
 }
 
   return (
     <>
+    <form onSubmit={onSubmits}>
+
       <div>
-        <h1>Hello {fullname} </h1>
+        <h1>Hello {firstname} {lastnamenew} </h1>
         <input  type="text"  placeholder='Enter your name' onChange={textChange} value ={text}/>
-        <button onClick={nameChange}>Click me</button>
+        <input  type="text"  placeholder='Enter your lastname' onChange={textChange2} value ={text2}/>
+        <button type='submit'>Click me</button>
       </div>
+      </form>
     </>
   );
 }
