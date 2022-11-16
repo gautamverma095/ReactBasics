9bac8a27038829895597bd92ec4cd0800495760c
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-16 22:54:45 +0530

    forms

--- Day11/forms/src/App.jsx
+++ Day11/forms/src/App.jsx
@@ -1,59 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-import React ,{useState}from "react"
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [fullname, setFullname] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+  });
 
-const [fullname,setFullname] = useState({
-  fname:"",
-  lname:""
-})
+  const textChange = (event) => {
+    // console.log(event.target.name);
+    const name = event.target.name;
+    const value = event.target.value;
 
-
-
-const textChange = (event) => {
-  // console.log(event.target.name);
-  const name = event.target.name
-  const value = event.target.value
-
-  setFullname((ddd) => {
-   
-    if(name === "fname")
-    {
-      return {
-        fname:value,
-    lname:ddd.lname
+    setFullname((ddd) => {
+      if (name === "fname") {
+        return {
+          fname: value,
+          lname: ddd.lname,
+          email: ddd.email
+        };
+      } else if (name === "lname") {
+        return {
+          fname: ddd.fname,
+          lname: value,
+          email: ddd.email
+        };
       }
-    }
-   else if(name === "lname")
-    {
-      return {
-        fname:ddd.fname,
-  lname:value
+      else if (name === "email") {
+        return {
+          fname: ddd.fname,
+          lname: ddd.lname,
+          email: value
+        };
       }
-    }
+    });
+  };
 
-  })
-
-}
-
-
-const onSubmits = (event) => {
-  event.preventDefault()
-
-}
+  const onSubmits = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <>
-    <form onSubmit={onSubmits}>
-
-      <div>
-        <h1>Hello {fullname.fname} {fullname.lname} </h1>
-        <input name='fname' type="text"  placeholder='Enter your name' onChange={textChange} value ={fullname.fname}/>
-        <input name='lname' type="text"  placeholder='Enter your lastname' onChange={textChange} value ={fullname.lname}/>
-        <button type='submit'>Click me</button>
-      </div>
+      <form onSubmit={onSubmits}>
+        <div>
+          <h1>
+            Hello {fullname.fname} {fullname.lname} {fullname.email} {" "}
+          </h1>
+          <input
+            name="fname"
+            type="text"
+            placeholder="Enter your name"
+            onChange={textChange}
+            value={fullname.fname}
+          />
+          <input
+            name="lname"
+            type="text"
+            placeholder="Enter your lastname"
+            onChange={textChange}
+            value={fullname.lname}
+          />
+          <input
+            name="email"
+            type="text"
+            placeholder="Enter email"
+            onChange={textChange}
+            value={fullname.email}
+          />
+          <button type="submit">Click me</button>
+        </div>
       </form>
     </>
   );
