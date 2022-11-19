618c38de9b5a127dd91485742761a07a61a199e1
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-19 11:52:35 +0530

    npm

---
+++ Day14/npm/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Clock from 'react-digital-clock';
+import React from 'react';
+
+
+function App() {
+  return (
+   <>
+           <Clock />
+           </>
+
+  );
+}
+
+export default App;
