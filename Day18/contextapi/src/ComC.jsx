d5e5fc2fc8a45f31bb689ade9b9bb05651cd7503
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-21 20:09:11 +0530

    contextapi

---
+++ Day18/contextapi/src/ComC.jsx
@@ -0,0 +1,27 @@
+import { FirstName,LastName } from "./App";
+
+function ComC() {
+    return (
+      <>
+        <FirstName.Consumer>
+            {
+                (fname) => {
+                   return (
+                       <LastName.Consumer>
+                           {
+                               (lname) => {
+                                   return  <h1>My name is {fname} {lname} </h1>
+                               }
+                           }
+                       </LastName.Consumer>
+                   )
+                }
+            }
+        </FirstName.Consumer>
+      </>
+    );
+  }
+  
+  export default ComC;
+
+ 
