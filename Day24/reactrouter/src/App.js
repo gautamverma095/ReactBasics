91674dafc06752bb26ef23984bf6a5760ec40b9d
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-23 12:56:38 +0530

    router

---
+++ Day24/reactrouter/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route  path='/' element = {<div>Home Page</div>}/>
+        <Route  path='/contact' element = {<div>Contact Page</div>}/>
+      </Routes>
+
+      </BrowserRouter>
+    </>
+    
+  );
+}
+
+export default App;
