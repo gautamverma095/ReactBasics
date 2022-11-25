80a8a98b0003983a819522b36a087d88408326c9
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-25 17:50:01 +0530

    1stprblm

--- day25/1stprblm/src/App.js
+++ day25/1stprblm/src/App.js
@@ -7,19 +7,23 @@ function App() {
 
   const [data,setData] = useState([])
   const [load,setLoad]  =useState(false)
+  const [page,setPage] = useState(1)
 
 
   useEffect(() => {
      renderData()
-  },[])
+  },[page])
 
+const handlePage = (num) => {
+  setPage(page+num)
 
+}
 
 const renderData = async () => {
 
   try{
     setLoad(true)
-    let res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
+    let res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`)
      res = await res.json()
      console.log(res);
      setData(res)
@@ -46,9 +50,15 @@ if(load)
 
    {
       data.map((el,index) => (
-        <li key={index}>{el.title}</li>
+        <li key={index}>{el.title} {"id -> "} {el.id}</li>
       ))
     }
+    <button disabled = {page == 1} onClick = {() => {
+      handlePage(-1)
+    }}>Prev</button>
+    <button disabled = {page == 10} onClick={ () => {
+      handlePage(1)
+    }}>Next</button>
     
     </div>
   );
