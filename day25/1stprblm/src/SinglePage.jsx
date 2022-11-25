3d44c47474c08b7874bb0d57daab1a97039c9a09
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-25 19:05:23 +0530

    1stprblm

--- day25/1stprblm/src/SinglePage.jsx
+++ day25/1stprblm/src/SinglePage.jsx
@@ -1,14 +1,13 @@
+const Singlepage = (props) => {
+  const { title, id } = props;
 
+  return (
+    <>
+      <li>
+        {id} {"-"} {title}
+      </li>
+    </>
+  );
+};
 
-  const Singlepage = (props) => {
-
-    const {title,id} = props
-
-    return (
-        <>
-        <li>{id} {"-"}  {title}</li>
-        </>
-    )
-}
-
-export default Singlepage
+export default Singlepage;
