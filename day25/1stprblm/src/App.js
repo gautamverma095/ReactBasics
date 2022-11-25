63f877cddfa54c9dd2d94a32cf3784896af840c6
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-25 19:02:53 +0530

    1stprblm

--- day25/1stprblm/src/App.js
+++ day25/1stprblm/src/App.js
@@ -1,65 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import  Singlepage  from "./SinglePage";
 
 function App() {
-
-  const [data,setData] = useState([])
-  const [load,setLoad]  =useState(false)
-  const [page,setPage] = useState(1)
-
+  const [data, setData] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-     renderData()
-  },[page])
+    renderData();
+  }, [page]);
 
-const handlePage = (num) => {
-  setPage(page+num)
+  const handlePage = (num) => {
+    setPage(page + num);
+  };
 
-}
-
-const renderData = async () => {
-
-  try{
-    setLoad(true)
-    let res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`)
-     res = await res.json()
-     console.log(res);
-     setData(res)
-     setLoad(false)
-
+  const renderData = async () => {
+    try {
+      setLoad(true);
+      let res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
+      );
+      res = await res.json();
+      setData(res);
+      setLoad(false);
+    } catch (error) {
+      console.log(error);
+      setLoad(false);
+    }
+  };
+  if (load) {
+    return <h1>Loading</h1>;
   }
-  catch(error){
-    console.log(error);
-    setLoad(false)
-  }
 
-}
-if(load)
-{
-  return <h1>Loading</h1>
-}
-  
-
- 
   return (
     <div className="App">
-    <h1>Posts</h1>
-    <button onClick={renderData}>Show data</button>
+      <h1>Posts</h1>
 
-   {
-      data.map((el,index) => (
-        <li key={index}>{el.title} {"id -> "} {el.id}</li>
-      ))
-    }
-    <button disabled = {page == 1} onClick = {() => {
-      handlePage(-1)
-    }}>Prev</button>
-    <button disabled = {page == 10} onClick={ () => {
-      handlePage(1)
-    }}>Next</button>
-    
+
+
+      {data.map((el,index) => {
+        return  <Singlepage title = {el.title}  key = {index} id = {el.id}/>
+      }) }
+      <button
+        disabled={page == 1}
+        onClick={() => {
+          handlePage(-1);
+        }}
+      >
+      
+        Prev
+      </button>
+      <h1>{page}</h1>
+      <button
+        disabled={page == 10}
+        onClick={() => {
+          handlePage(1);
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 }
---
+++ day25/1stprblm/src/SinglePage.jsx
@@ -0,0 +1,14 @@
+
+
+  const Singlepage = (props) => {
+
+    const {title,id} = props
+
+    return (
+        <>
+        <li>{id} {"-"}  {title}</li>
+        </>
+    )
+}
+
+export default Singlepage
