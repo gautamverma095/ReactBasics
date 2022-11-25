da0faf61a99b66a58d0537a1f24cb74bc1d1a20e
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-25 16:00:50 +0530

    1stprblm

--- day25/1stprblm/src/App.js
+++ day25/1stprblm/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+
+  const [num,setNum] = useState(0)
+
+  useEffect(() => {
+    document.title = `clicked me ${num} times`
+  },[])
+
+  const handle = () => {
+    setNum(num+1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={handle}>Clicked me {num} times</button>
     </div>
   );
 }
