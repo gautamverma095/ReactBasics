924f0ad03b0713d58b50da9830de7d458cda1d9d
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-26 20:20:25 +0530

    useref

---
+++ Day27/useref/src/Timer.jsx
@@ -0,0 +1,46 @@
+import { useRef } from "react"
+import { useState } from "react"
+
+
+const Timer = () => {
+
+    const [time,setTime] = useState(0)
+
+    const ref = useRef(null)
+
+    const onStart = () => {
+        if(ref.current !== null) return
+       
+      ref.current =   setInterval(() => {
+            console.log("aa");
+
+         setTime((preVal) => {
+             return preVal + 1
+         })
+            
+        }, 1000);
+    }
+
+
+    const onStop = () => {
+        clearInterval(ref.current)
+        ref.current = null
+    }
+
+    const onReset = () => {
+        setTime(0)
+        clearInterval(ref.current)
+    }
+    return (
+        <>
+     <h1>Timer</h1>
+        <h1>{time}</h1>
+        <button onClick={onStart}>Start</button>
+        <button onClick={onStop}>Stop</button>
+        <button onClick={onReset}>Reset</button>
+
+        </>
+    )
+}
+
+export default Timer
