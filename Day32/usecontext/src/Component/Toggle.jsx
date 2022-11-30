c81f2f23dae48c3e2a954bf7e4db2b9952cb8922
gautamverma095 <95958054+gautamverma095@users.noreply.github.com>
2022-11-30 10:14:20 +0530

    usecontext

--- Day32/usecontext/src/Component/Toggle.jsx
+++ Day32/usecontext/src/Component/Toggle.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { useContext } from 'react'
 import { AppContext } from '../Context/ContextProvider'
 
+
+
+
 const Toggle = () => {
 
-    const {toggleTheme} = useContext(AppContext)
+    const {toggleTheme,theme} = useContext(AppContext)
+
+  const style =   theme === "dark" ? {color:"red",backgroundColor:"green"} :{color:"pink",backgroundColor:"yellow"}
 
   return (
 
     <>
-    <button onClick={toggleTheme}>Toggle</button>
+    <button style={style} onClick={toggleTheme}>Toggle</button>
     </>
   )
 }
